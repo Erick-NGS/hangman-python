c94e681f20f8f3c3c99f84d46d69455283e30eeb
Erick Silva <engs.3003@gmail.com>
2024-04-24 23:45:04 -0300

    adding chars positions and replacement, adding new steps

--- main.py
+++ main.py
@@ -10,33 +10,35 @@ import random
 
 
 #STEP 2
-#TODO-1: - Create an empty List called display.
+#TODO-1: - Create an empty List called display. DONE
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-#TODO-2: - Loop through each position in the chosen_word;
+#TODO-2: - Loop through each position in the chosen_word; DONE
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_". DONE
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
 
 
 word_list = ["aardvark", "baboon", "camel"]
-
+display = []
 chosen_word = random.choice(word_list)
-
-letter_guess = input("Guess a letter: ").lower()
-
-for letter in chosen_word:
-    if letter_guess == letter:
-        print(letter_guess)
-    else:
-        print("_")
-
-
-
+guess_length = len(chosen_word)
 
 #Testing code
-#print(f'Pssst, the solution is {chosen_word}.')
+print(f'Pssst, the solution is {chosen_word}.')
+
+for character in range(guess_length):
+    display += "_"
+print(display)
+letter_guess = input("Guess a letter: ").lower()
+
+for pos in range(guess_length):
+    letter = chosen_word[pos]
+    if letter_guess == letter:
+        display[pos] = letter
+
+print(display)
