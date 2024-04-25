a51ef8766e3718b38d2db81687ed81ba521d870a
Erick Silva <engs.3003@gmail.com>
2024-04-25 14:45:26 -0300

    adding step 5 directives

--- main.py
+++ main.py
@@ -97,6 +97,20 @@ stages = ['''
 
 
 
+#STEP 5
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+
+#TODO-2: - Import the stages from hangman_art.py and make this error go away.
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+
+#TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
+#TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+
+
+
 word_list = ["aardvark", "baboon", "camel"]
 display = []
 chosen_word = random.choice(word_list)
